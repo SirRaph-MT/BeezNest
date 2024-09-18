549054aa7ac512a0fa20a70de9fee24729616fd2
pinkberry <pinkberry@SIRRAPH>
2024-09-18 15:26:45 +0100

    Add project files.

---
+++ BeezNest/Models/Entities/Admin.cs
@@ -0,0 +1,17 @@
+﻿namespace BeezNest.Models.Entities
+{
+    public class Admin
+    {
+        public Guid Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string phone { get; set; }
+
+
+    }
+}
